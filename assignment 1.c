f1904c425b3b3b841e3d63516bc3282cf173e4bf
sonu sharma <111167131+sonu6377@users.noreply.github.com>
2022-08-28 00:01:33 -0700

    Create assignment 1.c

---
+++ assignment 1.c
@@ -0,0 +1,53 @@
+/*#include<stdio.h>
+int main()
+{
+    int r;
+    float a;
+    printf("enter rediue of circle");
+    scanf("%d",&r);
+     a=3.14*r*r;
+     printf("area of circle %.1f",a);
+     return 0;
+}*/
+/*
+#include<stdio.h>
+int main()
+{
+    int x=printf("sonusharma");
+    printf("%d",x);
+    return 0;
+}*/
+/*
+#include<stdio.h>
+int main()
+{
+    printf("\"hello ,AMIT kumar\"");
+    return 0;
+}*/
+/*
+#include<stdio.h>
+int main()
+{
+     char a='\\',b='\\';
+    printf("\"%c%c\"",a,b);
+    return 0;
+}*/
+/*
+#include<stdio.h>
+int main()
+{
+    int day, month,year;
+    printf("enter your DD/mm/yyyy");
+    scanf("%d/%d/%d",&day,&month,&year);
+    printf("day-%d, month-%d,year-%d",day,month,year);
+    return 0;
+}*/
+#include<stdio.h>
+int main()
+{
+    int hour,mintue;
+    printf("enter time in hh:mm format");
+    scanf("%d:%d",&hour,&mintue);
+    printf("%d hour and %d mintue",hour,mintue);
+    return 0;
+}
